eeb2620aa0f89ad957c8068b126648ca8b07392b
Olesia Odyntsova <133907999+orlovaolesy@users.noreply.github.com>
2025-09-12 14:15:01 +0100

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -2,13 +2,13 @@ import yagmail
 import os
 
 def send_jobs_email(jobs, to_email, subject="Job Digest"):
-    body = "<h2>Новые вакансии</h2><ul>"
+    body = "<h2>New jobs</h2><ul>"
     for j in jobs:
         body += f"<li><strong>{j['title']}</strong> — {j['company']} [{j['source']}]<br>"
-        body += f"<a href='{j['link']}' target='_blank'>Открыть</a></li>"
-    body += "</ul>" if jobs else "<p>Новых вакансий не найдено.</p>"
+        body += f"<a href='{j['link']}' target='_blank'>Open</a></li>"
+    body += "</ul>" if jobs else "<p>No new vacancies found.</p>"
 
-    # ⬇️ вот тут самое главное
+    # ⬇️
     yag = yagmail.SMTP(
         user=os.getenv("GMAIL_USER"),
         password=os.getenv("GMAIL_APP_PASSWORD")
